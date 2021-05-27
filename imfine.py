772a2518f5b0d8371b0f9d59fa2ffa2d5b21446d
masankar666 <68776027+masankar666@users.noreply.github.com>
2021-05-27 17:46:39 +0530

    Add files via upload

---
+++ imfine.py
@@ -0,0 +1,32 @@
+print('task 1:')
+
+print("Definations:")
+print("Variable: Value stored in local data in a particular datatype,They are used as public,private and global and it access whenever call")
+print("function: It contain action given which is suitable on")
+print("Data type: It define the type of data stored in frontend variable and backend server ")
+print("operator: it is using which function will be used in program driven ,like as +,-,*,/,**,//,^,etc")
+print("Data structure: stored procedure")
+print("Inbuilt functions: It is already built in developer ,configure,run time it action module ")
+print("OOPS: obeject oriented programming using like polymorpism,inheritance ,etc")
+
+
+
+print('task 2:')
+
+print('i',"'",'m fine')
+
+Y = 4
+x = "sa"
+Z='vgp' 
+print(x, Y, Z)
+
+print('task 3:')
+a = 14
+b = 5
+c = abs(a % b)
+d = abs(a ** b)
+e = abs(a // b)
+pow = abs(a ** b)
+print(c)
+print(d)
+print(e)
