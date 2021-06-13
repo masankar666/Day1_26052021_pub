337e06c888f71df3076baa33c188d7286f9bbc2f
masankar666 <68776027+masankar666@users.noreply.github.com>
2021-06-13 13:25:14 +0530

    Dictionary Tasks

--- dict1.py
+++ dict1.py
@@ -1,4 +1,4 @@
-
+print ('#######-----Dictionary Tasks-----########')
 Dict = dict({1:["english","maths","science"], 2:[10,20,30], 3:["bio-botany","bio-zoology","Algebra"]})
 print("\nDictionary with the use of dict(): ")
 print(Dict)
@@ -16,42 +16,42 @@ print("####zoo")
 print(Dict[3][1][4:7])
 
 print ('#######----------########')
+print ('#######----------########')
 dict1 = {1:["english","maths","science",["history", "civics", "Geo-graphy",(45,46,47)]], 2:[10,[[20,[99]]],30], 3:("bio-botany","bio-zoology","Algebra")}
+
 #graphy
 print(dict1[1][3][2][4:])
 
 #46
-
 print(dict1[1][3][3][1])
+
 #99
 print(dict1[2][1][0][1][0])
+
 #20
 print(dict1[2][1][0][0])
+
 #botany
 print(dict1[3][0][4:])
-print ('#######----------########')
-
-
-
 
 print ('#######----------########')
+print ('#######----------########')
+
 myfamily = {"child1" : {"name" : "Emil","year" : 2004},"child2" : {"name" : "Tobias","year" : 2007},"child3" : {"name" : "Linus","year" : 2011}}
 
-
 print(myfamily)
-
 print(type(myfamily))
 
 
 print('name of child3')
 print(myfamily["child3"]["name"])
+
 print('year of child2')
 print(myfamily["child2"]["year"])
-print ('#########################')
-
-
 
 print ('#######----------########')
+print ('#######----------########')
+
 Task= {"name": "John","age": 30,"married": True,"divorced": False,"children": ("Ann","Billy"),"pets": None,"cars": [{"model1": "BMW 230", "mpg1": 27.5},{"model2": "Ford Edge", "mpg2": 24.1}]}
 
 print ('print Billy ?')
@@ -76,3 +76,5 @@ print ('print 7.5?')
 S = str(Task["cars"][0]["mpg1"])
 print(S[1:])
 
+print ('#######----------########')
+print ('#######-----The End-----########')
